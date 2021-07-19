f1ea8332d3dcdfb235f0f11594c02d835eb20260
Peter Koenders <pkoenders@gmail.com>
2021-07-19 15:21:15 +1200

    update prompt ref

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Header from '../components/header/'
 
+// markup
 const IndexPage = () => {
   return (
     <main>
@@ -11,7 +12,10 @@ const IndexPage = () => {
         <div className="content">
           <h1 className="presentation-title">Sharesight static interview prompt</h1>
           <p>
-            Prepared by <a href="https://pkoenders.netlify.app"> Peter Koenders</a> 19 July 2021
+            Prepared by <a href="https://pkoenders.netlify.app"> Peter Koenders</a> 19 July 2021 for{' '}
+            <a href="https://github.com/sharesight/static-interview-prompt">
+              this prompt - https://github.com/sharesight/static-interview-prompt"
+            </a>
           </p>
         </div>
       </section>
